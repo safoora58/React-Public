9dfcee27899879be6cf0142f8d81fd24ff257401
Reza Bazdaran <rbazdaran@gmail.com>
2024-05-14 19:46:00 +0330

    update Book.js

--- app/src/Components/Book/Book.js
+++ app/src/Components/Book/Book.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react'
 import './Book.css'
 
 export default class Book extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: this.props.title,
-      author: this.props.author,
-      year: this.props.year
-    }
-  }
+  
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     title: this.props.title,
+  //     author: this.props.author,
+  //     year: this.props.year
+  //   }
+  // }
   render() {
     return (
       <>
         <tr className='Book'>
           <td>{this.props.title}</td>
-          <td>{this.state.author}</td>
-          <td>{this.state.year}</td>
+          <td>{this.props.author}</td>
+          <td>{this.props.year}</td>
         </tr>
       </>
     )
